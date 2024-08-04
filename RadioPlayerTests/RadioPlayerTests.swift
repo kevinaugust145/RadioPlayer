79466f109faf794ba75bc39301e48fb260960e78
KevinHung <kevinaugust145@gmail.com>
2024-08-04 14:09:01 +0800

    Initial Commit

---
+++ RadioPlayerTests/RadioPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RadioPlayerTests.swift
+//  RadioPlayerTests
+//
+//  Created by 洪宗燦 on 2024/8/4.
+//
+
+import Testing
+@testable import RadioPlayer
+
+struct RadioPlayerTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

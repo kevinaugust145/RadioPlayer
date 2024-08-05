72c57b2767559383f24c0ecf0fb5f4ae21ac77a4
KevinHung <kevinaugust145@gmail.com>
2024-08-05 16:39:21 +0800

    2024/8/5

--- RadioPlayer/ViewController.swift
+++ RadioPlayer/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
+import AVKit
+
 class ViewController: UIViewController {
 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func Play(_ sender: Any) {
+        
+        let player = AVPlayer(url: URL(string: "http://live.leanstream.co/ICRTFM?args=tunein_aac")!)
+       player.play()
+    }
+    
 }
 
